9cdde8356b55ffdada25b0ef73257c10a89c8ddf
Pandaliangwei <Pandaliangwei@bingosoft.net>
2020-04-24 18:17:43 +0800

    配置修改

---
+++ bj-liangw-gateway/src/main/java/com/liangw/BjLiangwGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.liangw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjLiangwGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BjLiangwGatewayApplication.class, args);
+    }
+
+}
